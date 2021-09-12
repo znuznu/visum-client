eee76248eb1dcd643dd5223ecac052bc3033e1c1
znuznu <znu_1@protonmail.com>
2021-09-12 19:22:22 +0200

    feat: add HttpService, use it inside sign up page

--- src/components/SignUpBox/Form.tsx
+++ src/components/SignUpBox/Form.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useFormik } from 'formik';
+import { useMutation } from 'react-query';
 
+import { API_URL } from '../../config';
 import Button from '../common/Button';
 import Input from '../common/Input';
+import HttpService from '../../services/http';
 
 const StyledForm = styled.form`
   display: flex;
@@ -13,14 +16,14 @@ const StyledForm = styled.form`
 interface FormValues {
   username: string;
   password: string;
-  passwordConfirmation: string;
+  confirmPassword: string;
   registrationKey: string;
 }
 
 interface FormErrors {
   username?: string;
   password?: string;
-  passwordConfirmation?: string;
+  confirmPassword?: string;
   registrationKey?: string;
 }
 
@@ -35,13 +38,13 @@ const validate = (values: FormValues) => {
     errors.password = 'Required';
   }
 
-  if (!values.passwordConfirmation) {
-    errors.passwordConfirmation = 'Required';
+  if (!values.confirmPassword) {
+    errors.confirmPassword = 'Required';
   }
 
-  if (values.password !== values.passwordConfirmation) {
+  if (values.password !== values.confirmPassword) {
     errors.password = 'Password is different from the password confirmation';
-    errors.passwordConfirmation = 'Password is different from the password confirmation';
+    errors.confirmPassword = 'Password is different from the password confirmation';
   }
 
   if (!values.registrationKey) {
@@ -52,67 +55,85 @@ const validate = (values: FormValues) => {
 };
 
 const SignUpForm = () => {
+  const mutation = useMutation(
+    (account: FormValues) =>
+      HttpService.post(`${API_URL}/api/accounts/sign-up`, { json: account }),
+    {
+      onError: () => {
+        console.log('aie');
+      },
+      onSuccess: () => {
+        console.log('yes!');
+      }
+    }
+  );
+
   const formik = useFormik<FormValues>({
     initialValues: {
       username: '',
       password: '',
-      passwordConfirmation: '',
+      confirmPassword: '',
       registrationKey: ''
     },
     validate,
     onSubmit: (values) => {
-      console.log(values);
+      mutation.mutate(values);
     }
   });
 
   return (
-    <StyledForm onSubmit={formik.handleSubmit}>
-      <Input
-        id={'username'}
-        label={'Username'}
-        type={'text'}
-        error={
-          formik.touched.username && formik.errors.username
-            ? formik.errors.username
-            : undefined
-        }
-        {...formik.getFieldProps('username')}
-      />
-      <Input
-        id={'password'}
-        label={'Password'}
-        type={'password'}
-        error={
-          formik.touched.password && formik.errors.password
-            ? formik.errors.password
-            : undefined
-        }
-        {...formik.getFieldProps('password')}
-      />
-      <Input
-        id={'passwordConfirmation'}
-        label={'Password confirmation'}
-        type={'passwordConfirmation'}
-        error={
-          formik.touched.passwordConfirmation && formik.errors.passwordConfirmation
-            ? formik.errors.passwordConfirmation
-            : undefined
-        }
-        {...formik.getFieldProps('passwordConfirmation')}
-      />
-      <Input
-        id={'registrationKey'}
-        label={'Registration key'}
-        type={'registrationKey'}
-        error={
-          formik.touched.registrationKey && formik.errors.registrationKey
-            ? formik.errors.registrationKey
-            : undefined
-        }
-        {...formik.getFieldProps('registrationKey')}
-      />
-      <Button type="submit">{'Sign up'}</Button>
-    </StyledForm>
+    <>
+      {/* TODO Handle properly */}
+      {mutation.isError ? <div>An error occurred</div> : null}
+
+      <StyledForm onSubmit={formik.handleSubmit}>
+        <Input
+          id={'username'}
+          label={'Username'}
+          type={'text'}
+          error={
+            formik.touched.username && formik.errors.username
+              ? formik.errors.username
+              : undefined
+          }
+          {...formik.getFieldProps('username')}
+        />
+        <Input
+          id={'password'}
+          label={'Password'}
+          type={'password'}
+          error={
+            formik.touched.password && formik.errors.password
+              ? formik.errors.password
+              : undefined
+          }
+          {...formik.getFieldProps('password')}
+        />
+        <Input
+          id={'confirmPassword'}
+          label={'Password confirmation'}
+          type={'confirmPassword'}
+          error={
+            formik.touched.confirmPassword && formik.errors.confirmPassword
+              ? formik.errors.confirmPassword
+              : undefined
+          }
+          {...formik.getFieldProps('confirmPassword')}
+        />
+        <Input
+          id={'registrationKey'}
+          label={'Registration key'}
+          type={'registrationKey'}
+          error={
+            formik.touched.registrationKey && formik.errors.registrationKey
+              ? formik.errors.registrationKey
+              : undefined
+          }
+          {...formik.getFieldProps('registrationKey')}
+        />
+        <Button type="submit">{'Sign up'}</Button>
+      </StyledForm>
+    </>
   );
 };
 
