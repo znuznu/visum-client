d82927aba7e1aee376c5b64c53dc9c4c9d8fe50a
znuznu <znu_1@protonmail.com>
2022-01-30 15:46:36 +0100

    feat: MovieActionsPanel

--- src/components/MovieActionsPanel/index.tsx
+++ src/components/MovieActionsPanel/index.tsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import { useMutation } from 'react-query';
+import useAuthentication from '../../hooks/useAuthentication';
+import {
+  addMovieToWatchlist,
+  deleteMovie,
+  markMovieAsFavorite,
+  removeMovieFromFavorite,
+  removeMovieFromWatchlist
+} from '../../services/api/movie';
+
 import { AccessibleIcon } from '../common/AccessibleIcon';
 import {
   FavoriteFilledIcon,
@@ -14,37 +24,115 @@ interface MovieActionsPanelProps {
   isFavorite: boolean;
   isToWatch: boolean;
   onFavorite: () => void;
+  onRemoveFavorite: () => void;
+  onAddToWatchlist: () => void;
+  onRemoveFromWatchlist: () => void;
+  onDelete: () => void;
 }
 
-// TODO Handle onClick
 const MovieActionsPanel = ({
   movieId,
   isFavorite,
   isToWatch,
-  onFavorite
+  onFavorite,
+  onRemoveFavorite,
+  onAddToWatchlist,
+  onRemoveFromWatchlist,
+  onDelete
 }: MovieActionsPanelProps) => {
+  const { jwtToken } = useAuthentication();
+
+  const addToFavoriteMutation = useMutation(
+    (id: number) => markMovieAsFavorite({ authorization: `Bearer ${jwtToken}` }, id),
+    {
+      onError: (error) => {
+        // TODO #1
+        console.log(error);
+      },
+      onSuccess: () => {
+        onFavorite();
+      }
+    }
+  );
+
+  const removeFromFavoriteMutation = useMutation(
+    (id: number) => removeMovieFromFavorite({ authorization: `Bearer ${jwtToken}` }, id),
+    {
+      onError: (error) => {
+        // TODO #1
+        console.log(error);
+      },
+      onSuccess: () => {
+        onRemoveFavorite();
+      }
+    }
+  );
+
+  const addToWatchlistMutation = useMutation(
+    (id: number) => addMovieToWatchlist({ authorization: `Bearer ${jwtToken}` }, id),
+    {
+      onError: (error) => {
+        // TODO #1
+        console.log(error);
+      },
+      onSuccess: () => {
+        onAddToWatchlist();
+      }
+    }
+  );
+
+  const removeFromWatchlistMutation = useMutation(
+    (id: number) => removeMovieFromWatchlist({ authorization: `Bearer ${jwtToken}` }, id),
+    {
+      onError: (error) => {
+        // TODO #1
+        console.log(error);
+      },
+      onSuccess: () => {
+        onRemoveFromWatchlist();
+      }
+    }
+  );
+
+  const deleteMutation = useMutation(
+    (id: number) => deleteMovie({ authorization: `Bearer ${jwtToken}` }, id),
+    {
+      onError: (error) => {
+        // TODO #1
+        console.log(error);
+      },
+      onSuccess: () => {
+        onDelete();
+      }
+    }
+  );
+
   return (
     <StyledMovieActionsPanel>
       {isFavorite ? (
         <AccessibleIcon label="Set the movie as non-favorite">
-          <FavoriteFilledIcon />
+          <FavoriteFilledIcon
+            onClick={() => removeFromFavoriteMutation.mutate(movieId)}
+          />
         </AccessibleIcon>
       ) : (
         <AccessibleIcon label="Set the movie as a favorite">
-          <FavoriteIcon />
+          <FavoriteIcon onClick={() => addToFavoriteMutation.mutate(movieId)} />
         </AccessibleIcon>
       )}
       {isToWatch ? (
-        <AccessibleIcon label="Remove from the watch-list">
-          <ToWatchFilledIcon />
+        <AccessibleIcon label="Remove from the watchlist">
+          <ToWatchFilledIcon
+            onClick={() => removeFromWatchlistMutation.mutate(movieId)}
+          />
         </AccessibleIcon>
       ) : (
-        <AccessibleIcon label="Add to the watch-list">
-          <ToWatchIcon />
+        <AccessibleIcon label="Add to the watchlist">
+          <ToWatchIcon onClick={() => addToWatchlistMutation.mutate(movieId)} />
         </AccessibleIcon>
       )}
       <AccessibleIcon label="Remove the film">
-        <RemoveIcon />
+        <RemoveIcon onClick={() => deleteMutation.mutate(movieId)} />
       </AccessibleIcon>
     </StyledMovieActionsPanel>
   );
