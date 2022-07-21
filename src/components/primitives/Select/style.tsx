2c54e8ab2e97211c0ef24ea42f88492462f8af1e
znuznu <znu_1@protonmail.com>
2022-07-21 23:52:52 +0200

    build: update all radix-ui packages to 1.0.0 :tada:

--- src/components/primitives/Select/style.tsx
+++ src/components/primitives/Select/style.tsx
@@ -26,6 +26,7 @@ const StyledContent = styled(SelectPrimitive.Content)`
   border-radius: 6px;
   box-shadow: 0px 10px 38px -10px rgba(22, 23, 24, 0.35),
     0px 10px 20px -15px rgba(22, 23, 24, 0.2);
+  z-index: 6000;
 `;
 
 const StyledViewport = styled(SelectPrimitive.Viewport)`
